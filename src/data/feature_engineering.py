32ed2657aa2b58ef226ed68e01383e651c6e0f7b
AlyHafez <a.shoukry450@gmail.com>
2026-05-15 14:39:20 +0100

    add latitiude and longitude as features

--- src/data/feature_engineering.py
+++ src/data/feature_engineering.py
@@ -2,17 +2,20 @@ import pandas as pd # type: ignore[import]
 import numpy as np # type: ignore[import]
 import os
 from data_config import file_config
-def add_features(location:str)->pd.DataFrame:
+def add_features(location:str, lat:float, lon:float)->pd.DataFrame:
     """
     adds time-based features, daylight indicator, and normalized power output to the processed PVGIS data for a given location.
     Args:
         location (str): Name of the location (e.g., "London").
+        lat (float): Latitude of the location.
+        lon (float): Longitude of the location.
     Returns:
         pd.DataFrame: DataFrame with added features 
     """
     df = pd.read_csv(f"{file_config.processed_data_dir}/pvgis_{location.lower()}_processed.csv", parse_dates=["time"])
     df["location"] = location
-
+    df["lat"] = lat
+    df["lon"] = lon
     df["hour_sin"] = np.sin(2 * np.pi * df["time"].dt.hour / 24)
     df["hour_cos"] = np.cos(2 * np.pi * df["time"].dt.hour / 24)
 
@@ -39,7 +42,9 @@ if __name__ == "__main__":
     all_dfs = []
     for loc in file_config.locations:
         name = str(loc["name"])
-        df = add_features(name)
+        lat = loc["lat"]
+        lon = loc["lon"]
+        df = add_features(name, lat, lon)
         df.to_csv(f"{file_config.processed_data_dir}/pvgis_{name.lower()}_features.csv", index=False)
         print(f"Saved {name}: {df.shape}")
         all_dfs.append(df)
