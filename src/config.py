5fc3a0396395b9a09256c4279118029d8c1a4a9a
AlyHafez <a.shoukry450@gmail.com>
2026-05-18 02:23:51 +0100

    increase batch size

--- src/config.py
+++ src/config.py
@@ -31,9 +31,9 @@ class FileConfig:
 
 class ModelConfig:
     """Model hyperparameters and training settings."""
-    train_batch_size: int = 64
-    val_batch_size: int = 256
-    test_batch_size: int = 256
+    train_batch_size: int = 256
+    val_batch_size: int = 512
+    test_batch_size: int = 512
     max_encoder_length: int = 168
     max_prediction_length: int = 24
     hidden_size:int = 41
